3f5c7ae0cfbaf5868c463529929ead9765704234
R-Sharmi <159418503+R-Sharmi@users.noreply.github.com>
2025-09-07 00:04:31 -0700

    Add files via upload

---
+++ tcs/Employee.java
@@ -0,0 +1,22 @@
+package org.tcs;
+
+public class Employee {
+	private void empId() {
+		// TODO Auto-generated method stub
+		System.out.println("3081");
+	}
+	private void empName() {
+		// TODO Auto-generated method stub
+		System.out.println("suji");
+	}
+	private void empAddress() {
+		// TODO Auto-generated method stub
+		System.out.println("omr");
+	}
+public static void main(String[] args) {
+	Employee e=new Employee();
+	e.empId();
+	e.empName();
+	e.empAddress();
+}
+}
